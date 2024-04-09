e015256feb3f0a92487b2d20e1e512159d266fd6
My VO <kathy.nguyen198@gmail.com>
2024-04-09 14:08:16 +0200

    Buffering Data

--- output.py
+++ output.py
@@ -11,3 +11,28 @@ f.close()
 my_file = open("output.txt", "r+") # "r+" : read & write
 print (my_file.read())
 my_file.close()
+
+## Reading Between the Lines
+my_file = open("output.txt", "r")
+
+print (my_file.readline())
+print (my_file.readline())
+print (my_file.readline())
+my_file.close()
+
+## Buffering Data
+# During the I/O process, data is buffered: this means that it is held in a temporary location before being written to the file
+# Use a file handler to open a file for writing
+write_file = open("text.txt", "w")
+
+# Open the file for reading
+read_file = open("text.txt", "r")
+
+# Write to the file
+write_file.write("Not closing files is VERY BAD.")
+write_file.close()
+
+
+# Try to read from the file
+print (read_file.read())
+read_file.close()
